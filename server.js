55178de73b21bed550fec93de0f42a94a5d224fc
Stacee Ballback <staceeballback@gmail.com>
2018-03-25 15:58:06 -0700

    add index.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = express();
 const PORT = 3490;
 
 
-server.use(express,static(__dirname + '/assets'));
+server.use(express.static(__dirname + '/assets'));
 
 server.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -14,5 +14,5 @@ server.listen(PORT, () => {
   const now = new Date();
   console.log('-'.repeat(40));
   console.log('  ' + now.toLocaleString());
-  console.log('  ' + `Running on port ${PORT}'``);
+  console.log('  ' + `Running on port ${PORT}`);
 });
